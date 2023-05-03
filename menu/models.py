3f3ebcaf808bc475c99688a56d8602f834164ea4
Svavar Bjarki Bjarnarson <svavarbjarki@gmail.com>
2023-05-03 14:33:26 +0000

    menu tables

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Topping(models.Model):
+    TID = models.IntegerField(primary_key=True, unique=True)
+    name = models.CharField(max_length=255)
+
+class Pizza(models.Model):
+    PID = models.IntegerField(primary_key=True, unique=True)
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    image = models.CharField(max_length=9999)
+    description = models.CharField(max_length=9999)
+    spicy = models.BooleanField()
+    vegan = models.BooleanField()
+
+class PizzaToppings(models.Model):
+    PID = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    TID = models.ForeignKey(Topping, on_delete=models.CASCADE)
+
