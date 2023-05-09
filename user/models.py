cd5661d5461539006cefda0b14fce68ff3a9578b
Jon Arnar <jonarnar26@gmail.com>
2023-05-09 13:19:27 +0000

    profile

--- user/models.py
+++ user/models.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.models import User
 from django.db import models
+from menu.models import Pizza
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    favorite_pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
     profile_image = models.CharField(max_length=9999)
 
