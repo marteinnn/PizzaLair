a5d92fec39f42a8ece0804a060f00eed1e573f85
Svavar Bjarki Bjarnarson <svavarbjarki@gmail.com>
2023-05-09 13:24:37 +0000

    two for one added

--- deals/views.py
+++ deals/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from deals.models import Deals
+from menu.models import Pizza
 # Create your views here.
 def deals(request):
     return render(request, 'deals/index.html', {
         'alldeals': Deals.objects.all()
     })
+
+def twoforone(request):
+    pizzas = Pizza.objects.all()
+    return render(request, 'deals/two-for-one.html', {'pizzas': pizzas} )
